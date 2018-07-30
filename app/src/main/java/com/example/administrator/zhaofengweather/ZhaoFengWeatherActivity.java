dce5f80fffbbe65c16bfed866535b5d01400623b
binhe729 <hb335623291@163.com>
2018-07-30 16:42:21 +0800

    First commit.

---
+++ app/src/main/java/com/example/administrator/zhaofengweather/ZhaoFengWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.zhaofengweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhaoFengWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhao_feng_weather);
+    }
+}
